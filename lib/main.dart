0d180be23e9844c511ea4b6f81d339adaf09594c
jaugustin3 <jaugustin3@student.gsu.edu>
2024-12-07 23:08:39 -0500

    created buyer market page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'BuyerMarketplacePage.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -34,7 +36,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:BuyerMarketplacePage() ,
     );
   }
 }
